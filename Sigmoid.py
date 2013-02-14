5948c22736b7e29e9802ed11b143100a9c1bb238
Rahul Kavi <rahulkavi@gmail.com>
2013-02-13 22:32:36 -0500

    vectorized sigmoid function

--- Sigmoid.py
+++ Sigmoid.py
@@ -7,15 +7,5 @@ def sigmoidCalc(data):
 	'''
 	calculate the sigmoid of the given data
 	'''
-	data = np.array(data, dtype = np.longdouble)
-	g = np.zeros(data.shape, dtype = np.float64)
-	m,n = data.shape
-	#print data.dtype
-
-	
-	for eachRow in range(m):
-		for eachCol in range(n):
-			#print float(np.exp(-data[eachRow, eachCol]))
-			g[eachRow,eachCol] = (1.0) / (1.0 + (math.exp(-data[eachRow, eachCol])))
-	#print 'returning', g		
+	g = 1.0 / (1.0 + np.exp(-data))	
 	return g
